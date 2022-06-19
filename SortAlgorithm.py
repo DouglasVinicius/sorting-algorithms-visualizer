7721e7e94d330a2165e2a52b78e88f595080acb0
DouglasVinicius <dv.dullius@gmail.com>
2022-06-19 01:11:33 -0300

    Insertion sort first element fixed

--- SortAlgorithm.py
+++ SortAlgorithm.py
@@ -1,25 +1,4 @@
 class SortAlgorithm:
-    def __init__(self, dataset, sort_algorithm):
-        self.__dataset = dataset
-        self.__sort_algorithm
-
-    def insertion_sort(visualizer, ascending, dataset):
-        size = len(dataset)
-
-        for first_index in range(1, size):
-            current_value = dataset[first_index]
-            for second_index in range(first_index-1, 0, -1):
-                visualizer.comparisons += 1
-                visualizer.draw_dataset(second_index, first_index, first_index, True, True)
-                yield True
-
-                if((dataset[second_index] > current_value and ascending) or (dataset[second_index] < current_value and not ascending)):
-                    dataset[second_index+1] = current_value
-                    break
-                SortAlgorithm.swap(dataset, second_index, second_index+1)
-
-        return dataset
-
 
     def bubble_sort(visualizer, ascending, dataset):
         size = len(dataset)
@@ -28,11 +7,29 @@ class SortAlgorithm:
             for second_index in range(size-first_index-1):
                 visualizer.comparisons += 1
 
-                if((dataset[second_index] < dataset[second_index+1] and ascending) or (dataset[second_index] > dataset[second_index+1] and not ascending)):
+                if((dataset[second_index] > dataset[second_index+1] and ascending) or (dataset[second_index] < dataset[second_index+1] and not ascending)):
                     SortAlgorithm.swap(dataset, second_index, second_index+1)
 
                 visualizer.draw_dataset(second_index, size-first_index, second_index+1, True, False)
-            yield True
+                yield True
+        return dataset
+
+
+    def insertion_sort(visualizer, ascending, dataset):
+        size = len(dataset)
+
+        for first_index in range(1, size):
+            current_value = dataset[first_index]
+            for second_index in range(first_index-1, -1, -1):
+                visualizer.comparisons += 1
+                visualizer.draw_dataset(second_index, first_index, first_index, True, True)
+                yield True
+
+                if((dataset[second_index] < current_value and ascending) or (dataset[second_index] > current_value and not ascending)):
+                    dataset[second_index+1] = current_value
+                    break
+                SortAlgorithm.swap(dataset, second_index, second_index+1)
+
         return dataset
 
 
@@ -63,23 +60,3 @@ class SortAlgorithm:
 
     def swap(dataset, first_value_index, second_value_index):
         dataset[first_value_index], dataset[second_value_index] = dataset[second_value_index], dataset[first_value_index]
-
-
-#     def bubble_sort_test():
-#         dataset = []
-#         with open("random_file.dat") as file:
-#             for value in file.readlines():
-#                 dataset.append(int(value))
-#         size = len(dataset)
-#         print(dataset)
-#         for first_index in range(size-1):
-#             for second_index in range(size-first_index-1):
-
-#                 if(dataset[second_index] > dataset[second_index+1]):
-#                     SortAlgorithm.swap(dataset, second_index, second_index+1)
-
-#         print(dataset)
-
-
-
-# SortAlgorithm.bubble_sort_test()
